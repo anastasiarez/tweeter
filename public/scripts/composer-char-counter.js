b919c785d73014e7488823c56bf4d9830fc2f810
Mona Lisa <anastasiia.rn@gmail.com>
2023-07-09 21:12:05 -0400

    implement counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').keydown(function(e) {
+    const maxLength = 140;
+    let currentLength = $(this).val().length + 1;
+    $('.counter').text(maxLength - currentLength);
+    if (currentLength > maxLength) {
+      console.log(e);
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').text(maxLength - currentLength);
+      $('.counter').css('color', 'black');
+    }
+    console.log(maxLength - currentLength);
+  });
+});
