9dd4a17a8cc66a39019d57297e618ff7d50ff790
Mona Lisa <anastasiia.rn@gmail.com>
2023-07-15 12:18:43 -0400

    implementing renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+function renderTweets(tweets) {
+  const $tweetsContainer = $('#tweets-container');
+
+  $tweetsContainer.empty();
+
+  tweets.forEach((tweet) => {
+    const $tweet = createTweetElement(tweet);
+    $tweetsContainer.append($tweet);
+  });
+}
+
 
 function createTweetElement(tweet) {
   const { name, avatars, handle } = tweet.user;
   const { text } = tweet.content;
-  const createdAt = new Date(tweet.created_at);
 
   const $tweet = $(`
   <article class="tweet">
   <div>
-      <header id="tweet-header">
-      <img class="user-icon" srs="${avatars}" alt="User Avatar">
-      <h4>${name}</h4>
+  <header id="tweet-header">
+  <img class="user-icon" src="${avatars}" alt="User Avatar">
+  <h4>${name}</h4>
   </div>
   <p>${handle}</p>
   </header>
@@ -23,6 +60,7 @@ function createTweetElement(tweet) {
   <p>${text}</p>
   </div>
   <footer>
+  const createdAt = new Date(tweet.created_at);
   <p>${timeago.format(createdAt)}</p>
   <div clas="icons">
   <i class="fas fa-comment"></i>
@@ -30,24 +68,9 @@ function createTweetElement(tweet) {
   <i class="fas fa-heart"></i>
   </div>
   </footer>
-</article>
+  </article>
   `);
   return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
-
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac"
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
-  },
-  created_at: 1461116232227
-};
-
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); 
+renderTweets(data);
