2efaf1ad7d8c20e117a24a214013adac7bb86fdf
Mona Lisa <anastasiia.rn@gmail.com>
2023-07-19 17:50:08 -0400

    implement Validation Errors Display

--- public/scripts/char-counter.js
+++ public/scripts/char-counter.js
@@ -1,18 +1,39 @@
 // function to track tweet char lengths
 
 $(document).ready(function() {
+  function showError(message) {
+    $('#error-messages').text(message).slideDown();
+  }
+
+  function hideError() {
+    $('#error-messages').slideUp();
+  }
+
   $('.new-tweet textarea').on('input', function() {
-    const maxLength = 140;
+    const MAX_LENGTH = 140;
     const currentLength = $(this).val().length;
-    console.log(currentLength);
-    const remainingChars = maxLength - currentLength;
-    
+    const remainingChars = MAX_LENGTH - currentLength;
+
+    // Update the character counter
     $('.counter').text(remainingChars);
-    
-    if (remainingChars < 0) {
-      $('.counter').addClass('counter-exceeded');
+
+    // Check if textarea is empty or character limit is exceeded
+    if (currentLength === 0 || remainingChars < 0) {
+
+      if (currentLength > 140) {
+        showError('This message is exceeding the maximum length of 140 characters. Please try again.');
+      }
+
+      // Disable the button
+      $('#submit').attr('disabled', true);
     } else {
-      $('.counter').removeClass('counter-exceeded');
+      // Enable the button
+      $('#submit').attr('disabled', false);
+      hideError()
     }
+
+    // Toggle the 'counter-exceeded' class based on the character limit
+    $('.counter').toggleClass('counter-exceeded', remainingChars < 0);
   });
 });
+
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,4 @@
-
 $(document).ready(function() {
-
-  // function getHourDiff(start, end) {
-  //   return Math.floor((end - start) / 3600000);
-  // }
-
 
   // 1 - make a call to the server & show the tweets
 
@@ -17,8 +11,7 @@ $(document).ready(function() {
     });
   }
 
-
-  // 2 - Add a tweet to a container (empty the containerer from any previous information, create tweet using a f-n, and add it to a container)
+  // 2 - Add a tweet to a container (empty the container from any previous information, create tweet using a function, and add it to a container)
 
   function renderTweets(tweets) {
     const $tweetsContainer = $('#tweets-container');
@@ -29,69 +22,67 @@ $(document).ready(function() {
     });
   }
 
-
   function createTweetElement(tweet) {
     const { name, avatars, handle } = tweet.user;
     const { text } = tweet.content;
     const createdAt = new Date(tweet.created_at);
 
     const $tweet = `
-    
-
-  <article class="tweet">
-  <div>
-    <header id="tweet-header">
-      <img class="user-icon" src="${avatars}" alt="User Avatar">
-      <h4>${name}</h4>
+    <article class="tweet">
+      <div>
+        <header id="tweet-header">
+          <img class="user-icon" src="${avatars}" alt="User Avatar">
+          <h4>${name}</h4>
+          </div>
+        <p class="handle">${handle}</p>
+      </header>
+      <div class="tweet-content">
+        <p>${escapeHTML(text)}</p>
       </div>
-    <p class="handle">${handle}</p>
-  </header>
-  <div class="tweet-content">
-    <p>${escapeHTML(text)}</p>
-  </div>
 
-  <footer id="tweet-footer">
- <p class="timestamp" title="${createdAt.toISOString()}">${timeago.format(createdAt)}</p>
-  
-  <div class="icons">
-  <i class="fas fa-comment"></i>
-  <i class="fas fa-retweet"></i>
-  <i class="fas fa-heart"></i>
-  </div>
-  </footer>
-  </article>
-  `;
-
+      <footer id="tweet-footer">
+     <p class="timestamp" title="${createdAt.toISOString()}">${timeago.format(createdAt)}</p>
+      <div class="icons">
+      <i class="fas fa-comment"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      </div>
+      </footer>
+      </article>
+    `;
     return $tweet;
   }
 
-  function registerSubmit() {
+  function sendTweet(event) {
+    event.preventDefault();
+    console.log('event.target', event.target);
 
-    function sendTweet(event) {
-      event.preventDefault();
-      console.log('event.target', event.target);
+    const formData = $(event.target).serialize();
 
-      const formData = $(event.target).serialize();
-
-      $.ajax({
-        url: "/tweets",
-        method: 'post',
-        data: formData
-      }).done(function() {
-        fetchTweets();
-      });
-    }
-    $('#form').submit(sendTweet);
+    $.ajax({
+      url: "/tweets",
+      method: 'post',
+      data: formData
+    }).done(function() {
+      $('#tweet-text').val('')
+      fetchTweets();
+    })
   }
-
   function escapeHTML(text) {
     const element = document.createElement('div');
     element.innerText = text;
     return element.innerHTML;
   }
+  function registerSubmit() {
+    $('#form').submit(sendTweet);
+  }
+
+  // Call the functions to fetch tweets, register submit event, and apply timeago plugin
 
   fetchTweets();
   registerSubmit();
+
   $("p.timestamp").timeago();
 });
+
 
