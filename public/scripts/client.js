37230fcf7693e90e6d96aaf775fa68b96c7d393d
Mona Lisa <anastasiia.rn@gmail.com>
2023-07-18 21:09:13 -0400

    fixing bugs in timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,68 +1,61 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
-function getHourDiff(start, end) {
-  return Math.floor((end - start) / 3600000);
-}
+$(document).ready(function() {
+
+  // function getHourDiff(start, end) {
+  //   return Math.floor((end - start) / 3600000);
+  // }
 
 
+  // 1 - make a call to the server & show the tweets
 
-// 1 - make a call to the server & show the tweets
-
-function fetchTweets() {// get all tweets from the server
-  $.ajax({
-    url: "/tweets",
-    method: 'get'
-  }).done(data => {// data is all tweets
-    console.log('TWEETS:', data);
-    renderTweets(data);
-  });
-}
-
-fetchTweets();
+  function fetchTweets() {
+    $.ajax({
+      url: "/tweets",
+      method: 'get'
+    }).done(data => {
+      renderTweets(data);
+    });
+  }
 
 
-// 2 - Add a tweet to a container (empty the containerer from any previous information, create tweet using a f-n, and add it to a container)
-
-function renderTweets(tweets) {
-  const $tweetsContainer = $('#tweets-container');
-  $tweetsContainer.empty();
-  tweets.forEach((tweet) => {
-    const $tweet = createTweetElement(tweet);
-    $tweetsContainer.append($tweet);
-  });
-}
 
 
-function createTweetElement(tweet) {
-  const { name, avatars, handle } = tweet.user;
-  const { text } = tweet.content;
-  const createdAt = new Date(tweet.created_at).getSeconds();
-  const rtf1 = new Intl.RelativeTimeFormat('en', { style: 'short' });
+  // 2 - Add a tweet to a container (empty the containerer from any previous information, create tweet using a f-n, and add it to a container)
+
+  function renderTweets(tweets) {
+    const $tweetsContainer = $('#tweets-container');
+    $tweetsContainer.empty();
+    tweets.forEach((tweet) => {
+      const $tweet = createTweetElement(tweet);
+      $tweetsContainer.append($tweet);
+    });
+  }
 
 
-  const timeEnd = new Date().getSeconds();
-  const hourDiff = getHourDiff(timeEnd, createdAt);
+  function createTweetElement(tweet) {
+    const { name, avatars, handle } = tweet.user;
+    const { text } = tweet.content;
 
-  const $tweet = `
+    const createdAt = new Date(tweet.created_at);
+
+    const $tweet = `
   <article class="tweet">
   <div>
   <header id="tweet-header">
   <img class="user-icon" src="${avatars}" alt="User Avatar">
   <h4>${name}</h4>
   </div>
-  <p>${handle}</p>
+  <p class="handle">${handle}</p>
   </header>
   <div class="tweet-content">
   <p>${text}</p>
   </div>
-  <footer>
   
-  <p>${rtf1.format(hourDiff, 'hours')}</p>
-  <div clas="icons">
+
+  <footer id="tweet-footer">
+ <p class="timestamp" title="${createdAt.toISOString()}">${timeago.format(createdAt)}</p>
+  
+  <div class="icons">
   <i class="fas fa-comment"></i>
   <i class="fas fa-retweet"></i>
   <i class="fas fa-heart"></i>
@@ -71,28 +64,29 @@ function createTweetElement(tweet) {
   </article>
   `;
 
-  return $tweet;
-}
-
-
-function registerSubmit() {
-
-  function sendTweet(event) {
-    event.preventDefault();
-    console.log('event.target', event.target);
-
-    const formData = $(event.target).serialize();
-
-    $.ajax({
-      url: "/tweets",
-      method: 'post',
-      data: formData
-    }).done(function() {
-      fetchTweets();
-    });
+    return $tweet;
   }
-  $('#form').submit(sendTweet);
-}
 
-registerSubmit();
+  function registerSubmit() {
 
+    function sendTweet(event) {
+      event.preventDefault();
+      console.log('event.target', event.target);
+
+      const formData = $(event.target).serialize();
+
+      $.ajax({
+        url: "/tweets",
+        method: 'post',
+        data: formData
+      }).done(function() {
+        fetchTweets();
+      });
+    }
+    $('#form').submit(sendTweet);
+  }
+
+  fetchTweets();
+  registerSubmit();
+  $("p.timestamp").timeago();
+});
